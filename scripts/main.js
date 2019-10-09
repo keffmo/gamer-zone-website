29c691d72adb9e2c953c17c4c3fbc76bfca701da
keffmo <keffspace@gmail.com>
2019-10-09 11:59:59 -0600

    rewrote code to a more sassy style, added back-to-top button, updated readme revision

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,24 @@ $(".cf a").on("click", function(event) {
     );
   }
 });
+
+// Back to top button functions
+//Get the button:
+mybutton = document.getElementById("back-to-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function back2topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
